8aa0298fd082297611f1343e67a5f17cfe6a060d
Rafael Pachehco <ravpacheco>
2016-01-19 14:35:30 -0200

    Add a very basic deadlock example

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sandbox
 This repository is only to exemplify some basic concepts.
+
+### [Array Binary Search](https://github.com/ravpacheco/sandbox/tree/master/src/ArrayBinarySearch/ArrayBinarySearch)
+
+an efficient search algorithm with O(log(n)) time complexity
+
+### [Deadlock example](https://github.com/ravpacheco/sandbox/tree/master/src/ArrayBinarySearch/ArrayBinarySearch)
+
+code fragment to exemplify a deadlock
---
+++ src/DeadlockExample/DeadlockExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlockExample
+{
+    class Program
+    {
+
+        private static int _mutex = 0;
+
+        //Exemplify on very basic case of deadlock (only for study propose)
+        static void Main(string[] args)
+        {
+            var taskP1 = GetP1();
+            var taskP2 = GetP2(taskP1);
+
+            Task.Run(() => taskP2);
+        }
+
+        static Task<int> GetP1()
+        {
+            //State A of P1
+            //Code to exemplify some resource that dont respond until P2 change to state B
+            while (_mutex == 0);
+
+            //State B of P1 process is return value 0;
+            return Task.FromResult(0);
+        }
+
+        static async Task<int> GetP2(Task<int> resource)
+        {
+            //State A of P2 is wait for P1 finish (stay on state B)
+            var result = await resource;
+
+
+            //State B of P2 process is change mutex and print P1 result;
+            _mutex = 1;
+            Console.WriteLine($"P2 execute your job and get P1 result: {result}");
+
+            return result;
+        }
+    }
+}
